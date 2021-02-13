b327ef0293d20350be212bdeee21b9a6d8e829c2
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2021-02-13 12:36:36 -0800

    Added While Loop

--- Calculator.cpp
+++ Calculator.cpp
@@ -58,6 +58,16 @@ int main() {
 }
 
 int calculate(Operator mathOperator, int firstOperand, int secondOperand) {
+	//while loop that allows the user to continue entering calculations until
+	//0+0 is entered.
+
+	//We do not need to do that part however, as that code
+	//has already been provided to us.
+
+	while (mathOperator)
+	{
+		/* code */
+	}
 	
 	return 0;
 }
